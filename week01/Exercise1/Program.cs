5ec975921ec448b177a85b6f2613bd4e5230e189
Willian-byu-pro <willianramosdida@gmail.com>
2025-07-07 22:08:09 -0300

    add

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+
+
+        Console.Write("What is your first name ?");
+        string name = Console.ReadLine();
+        Console.Write("What is your last name?");
+        string lastname = Console.ReadLine();
+
+        Console.Write($"Your name is {lastname}, {name} {lastname}");
+        
+
+
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,93 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your percentage?");
+        string porcent = Console.ReadLine();
+
+        int answer = int.Parse(porcent);
+
+
+
+        String letter = "";
+        string sig = "";
+
+
+
+
+        if (answer >= 90)
+        {
+            letter = "A";
+            int teg = answer % 10;
+            if (teg < 3)
+            {
+                sig = "- ";
+            }
+
+        }
+
+        else if (answer >= 80)
+        {
+            letter = "B";
+            int teg = answer % 10;
+            if (teg >= 7)
+            {
+                sig = "+ ";
+            };
+            if (teg < 3)
+            {
+                sig = "- ";
+            }
+
+        }
+
+        else if (answer >= 70)
+        {
+            letter = "C";
+            int teg = answer % 10;
+            if (teg >= 7)
+            {
+                sig = "+ ";
+            }
+            if (teg < 3)
+            {
+                sig = "- ";
+            }
+
+        }
+
+        else if (answer >= 60)
+        {
+            letter = "D";
+            int teg = answer % 10;
+            if (teg >= 7)
+            {
+                sig = "+ ";
+            }
+            if (teg < 3)
+            {
+                sig = "- ";
+            }
+
+        }
+
+        else if (answer < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is:{letter}{sig}");
+
+        if (answer >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }        
+        
+
+
     }
 }
