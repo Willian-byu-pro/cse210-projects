e3286bbe0a510ac0a018cff2a4874f233055ee3d
Willian-byu-pro <willianramosdida@gmail.com>
2025-07-11 22:05:48 -0300

    exc4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,73 @@
 using System;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Resources;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        Console.Write("Enter number:");
+        string resp = Console.ReadLine();
+        int answer = int.Parse(resp);
+        int largeNumber = 0;
+
+        List<int> numbers = new List<int>();
+
+
+
+        while (answer != 0)
+        {
+            if (answer != 0)
+            {
+                numbers.Add(answer);
+    
+
+            }
+
+            Console.Write("Enter number:");
+            resp = Console.ReadLine();
+            answer = int.Parse(resp);
+
+
+
+
+
+
+
+
+        }
+
+        double SumValuer = 0;
+
+
+        foreach (int numb in numbers)
+        {
+            SumValuer += numb;
+
+            if (numb > largeNumber)
+            {
+                largeNumber = numb;
+            }
+
+        }
+
+        double averege = SumValuer / numbers.Count;
+
+        Console.WriteLine($"The sum is: {SumValuer}");
+        Console.WriteLine($"The average is: {averege}");
+        Console.WriteLine($"The largest number is: {largeNumber}");
+        
+        
+
+
+
+
     }
+       
+
+
 }
