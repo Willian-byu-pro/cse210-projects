99eae1dc591f6c67a7c56144d23473d2f9fa0182
Willian-byu-pro <willianramosdida@gmail.com>
2025-07-28 20:08:20 -0300

    Add project video

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+       {
+        List<Video> videos = new List<Video>();
+
+        // Video 1
+        Video video1 = new Video("How to Make Homemade Bread", "Maria's Kitchen", 320);
+        video1.AddComment(new Comment("John", "I loved the recipe!"));
+        video1.AddComment(new Comment("Clara", "It turned out delicious here at home."));
+        video1.AddComment(new Comment("Lucas", "You explain things really well!"));
+        videos.Add(video1);
+
+        // Video 2
+        Video video2 = new Video("Top 5 Games of 2025", "GameNerd", 780);
+        video2.AddComment(new Comment("Peter", "You forgot Elden Ring 2, huh!"));
+        video2.AddComment(new Comment("Anna", "Loved the video editing."));
+        video2.AddComment(new Comment("James", "All amazing games!"));
+        videos.Add(video2);
+
+        // Video 3
+        Video video3 = new Video("How to Learn English Fast", "Teacher Julia", 450);
+        video3.AddComment(new Comment("Ralph", "I'm going to apply these tips."));
+        video3.AddComment(new Comment("Matthew", "Very good!"));
+        video3.AddComment(new Comment("Laura", "I'm already seeing results."));
+        videos.Add(video3);
+
+        // Display everything in the console
+        foreach (Video video in videos)
+        {
+            Console.WriteLine($"Title: {video._title}");
+            Console.WriteLine($"Author: {video._author}");
+            Console.WriteLine($"Length: {video._length} seconds");
+            Console.WriteLine($"Number of comments: {video.GetNumberOfComments()}");
+            Console.WriteLine("Comments:");
+            foreach (Comment comment in video.GetComments())
+            {
+                Console.WriteLine($"- {comment._name}: {comment._text}");
+            }
+            Console.WriteLine("--------------------------------");
+        }
+    }
     }
 }
---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,29 @@
+public class Video
+{
+    public string _title { get; set; }
+    public string _author { get; set; }
+    public int _length { get; set; } // em segundos
+    private List<Comment> _comments = new List<Comment>();
+
+    public Video(string title, string author, int length)
+    {
+        _title = title;
+        _author = author;
+        _length = length;
+    }
+
+    public void AddComment(Comment comment)
+    {
+        _comments.Add(comment);
+    }
+
+    public int GetNumberOfComments()
+    {
+        return _comments.Count;
+    }
+
+    public List<Comment> GetComments()
+    {
+        return _comments;
+    }
+}
